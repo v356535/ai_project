bafea31ad01ff41ea9ab4c03eec8fd81d384dced
v356535 <virgilmaiden@gmail.com>
2025-12-31 04:49:05 -0500

    initial commit: GGemini API integration

---
+++ main.py
@@ -0,0 +1,23 @@
+import os
+from google import genai
+from dotenv import load_dotenv
+
+load_dotenv()
+
+api_key = os.environ.get("GEMINI_API_KEY")
+client = genai.Client(api_key='AIzaSyC_wure4XPZOfliGvrrsz5IBySRf8ipso8')
+
+if api_key == None:
+    raise RuntimeError("API key not found")
+
+response = client.models.generate_content(
+        model='gemini-2.5-flash', contents='Why is Boot.dev such a great place to learn backend development? Use one paragraph maximum.')
+
+print(response.text)
+
+def main():
+
+
+
+	if __name__ == "__main__":
+    		main()
