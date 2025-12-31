6225b52087f5517e3008b3a2c1d5176f323fca65
v356535 <virgilmaiden@gmail.com>
2025-12-31 06:43:38 -0500

    tokens counted

--- main.py
+++ main.py
@@ -2,22 +2,26 @@ import os
 from google import genai
 from dotenv import load_dotenv
 
-load_dotenv()
+def main():
+	load_dotenv()
 
-api_key = os.environ.get("GEMINI_API_KEY")
-client = genai.Client(api_key=api_key)
+	api_key = os.environ.get("GEMINI_API_KEY")
+	client = genai.Client(api_key=api_key)
 
-if api_key == None:
-    raise RuntimeError("API key not found")
+	if api_key == None:
+    		raise RuntimeError("API key not found")
 
-response = client.models.generate_content(
+	response = client.models.generate_content(
         model='gemini-2.5-flash', contents='Why is Boot.dev such a great place to learn backend development? Use one paragraph maximum.')
 
-print(response.text)
+	print(response.text)
 
-def main():
+	if response is None or response.usage_metadata is None:
+		print("Failed API request")
+		return
+	print(f"Prompt tokens: {response.usage_metadata.prompt_token_count}")
+	print(f"Response tokens: {response.usage_metadata.candidates_token_count}")
+
+main()
 
 
-
-	if __name__ == "__main__":
-    		main()
