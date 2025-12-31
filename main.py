4220152d41e8aabca6ca4421f10cfe1d4b09998c
v356535 <virgilmaiden@gmail.com>
2025-12-31 05:17:25 -0500

    Use .env for gemini API key and ignore secrets

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ from dotenv import load_dotenv
 load_dotenv()
 
 api_key = os.environ.get("GEMINI_API_KEY")
-client = genai.Client(api_key='AIzaSyC_wure4XPZOfliGvrrsz5IBySRf8ipso8')
+client = genai.Client(api_key=api_key)
 
 if api_key == None:
     raise RuntimeError("API key not found")
