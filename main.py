a1e0fb6e398a624d7b959010d904cc316219aa21
v356535 <virgilmaiden@gmail.com>
2026-01-01 03:30:31 -0500

    added content to messages

--- main.py
+++ main.py
@@ -1,20 +1,29 @@
 import os
 from google import genai
 from dotenv import load_dotenv
-
+from google.genai import types
+import argparse
 def main():
 	load_dotenv()
 
 	api_key = os.environ.get("GEMINI_API_KEY")
 	client = genai.Client(api_key=api_key)
 
+
+
 	if api_key == None:
     		raise RuntimeError("API key not found")
 
+	parser = argparse.ArgumentParser(description="Chatbot")
+	parser.add_argument("user_prompt", type=str, help="User prompt")
+	args = parser.parse_args()
+	messages = [types.Content(role="user", parts=[types.Part(text=args.user_prompt)])]
 	response = client.models.generate_content(
-        model='gemini-2.5-flash', contents='Why is Boot.dev such a great place to learn backend development? Use one paragraph maximum.')
+        model='gemini-2.5-flash', contents=messages)
 
 	print(response.text)
+
+
 
 	if response is None or response.usage_metadata is None:
 		print("Failed API request")
